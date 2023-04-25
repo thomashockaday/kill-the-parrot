e430711613cb2175f73418ab75f350d17cabb9b2
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2023-04-25 20:03:45 +0100

    Animate canvas with requestAnimationFrame

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
+class Parrot {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.position = {
+      x: this.canvas.width / 2,
+      y: this.canvas.height / 2,
+    };
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 450;
 
-ctx.fillRect(10, 10, 100, 100);
+const parrot = new Parrot(canvas);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  parrot.draw(ctx);
+
+  parrot.position.x++;
+}
+
+window.addEventListener("load", () => {
+  requestAnimationFrame(animate);
+});
